46ab17801dcb0e448a556222182257c8792d1acf
amorozov <amorozov@am-air.local>
2023-02-03 13:32:57 +0300

    Initial commit

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'react/function-component-definition': [1, { namedComponents: 'arrow-function' }],
+  },
+}
---
+++ app/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="App">
+    <h1> Memo game </h1>
+  </div>
+)
+
+export default App
