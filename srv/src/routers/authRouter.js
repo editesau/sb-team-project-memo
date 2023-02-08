923b9ff1aa981988240bb7c363e1cb859b92b546
editesau <alexey.po121@gmail.com>
2023-02-08 11:13:51 +0300

    add cors and morgan package

---
+++ srv/src/routers/authRouter.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+export const authRouter = Router()
+
+authRouter.post('/signin')
+authRouter.post('/signup')
+authRouter.get('/signout')
+authRouter.get('/refresh')
