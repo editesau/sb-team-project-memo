4c78b046826f163cb5c868b2542b14659c9f539c
editesau <alexey.po121@gmail.com>
2023-02-08 18:47:31 +0300

    Add createUser description

--- srv/src/controllers/authController.js
+++ srv/src/controllers/authController.js
@@ -1,5 +1,14 @@
 import { dbCreateUser } from '../db/dbActions.js'
 
+/** Create user with parameters provided in request body in JSON
+ * send statuses to client:
+ * 200 - user created, body {_id, email}
+ * 409 - user already exists, body {message}
+ * 500 - another DB error, body {message}
+ * @param req request object from express
+ * @param res response to send client
+ * @return {Promise}
+ */
 export const userCreate = async (req, res) => {
   const { email, password } = req.body
   try {
@@ -8,9 +17,9 @@ export const userCreate = async (req, res) => {
     res.status(200).json(userData)
   } catch (dbError) {
     if (dbError.message.indexOf('duplicate')) {
-      res.status(400).json({ message: 'User already exists' })
+      res.status(409).json({ message: 'User already exists' })
     } else {
-      res.status(500).json(dbError.message)
+      res.status(500).json({ message: dbError.message })
     }
   }
 }
