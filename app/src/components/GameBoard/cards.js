d5240cca2442828be34eeadce67e5dc630b22b90
RamilKhos <ramil091292@gmail.com>
2023-02-14 23:42:36 +0400

    add gameboard

---
+++ app/src/components/GameBoard/cards.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-relative-packages */
+import logo1 from '../../../../srv/resources/cards/animals/bear.png'
+import logo2 from '../../../../srv/resources/cards/animals/chameleon.png'
+import logo3 from '../../../../srv/resources/cards/animals/deer.png'
+import logo4 from '../../../../srv/resources/cards/animals/fox.png'
+import logo5 from '../../../../srv/resources/cards/animals/dog.png'
+import logo6 from '../../../../srv/resources/cards/animals/horse.png'
+import logo7 from '../../../../srv/resources/cards/animals/panda.png'
+import logo8 from '../../../../srv/resources/cards/animals/lion.png'
+import logo9 from '../../../../srv/resources/cards/animals/squirrel.png'
+
+export const cardsFromBD = [
+  { id: 1, logo: logo1, number: 1 },
+  { id: 2, logo: logo2, number: 2 },
+  { id: 3, logo: logo3, number: 3 },
+  { id: 4, logo: logo4, number: 4 },
+  { id: 5, logo: logo5, number: 5 },
+  { id: 6, logo: logo6, number: 6 },
+  { id: 7, logo: logo7, number: 7 },
+  { id: 8, logo: logo8, number: 8 },
+  { id: 9, logo: logo9, number: 9 },
+
+  { id: 1, logo: logo1, number: 10 },
+  { id: 2, logo: logo2, number: 11 },
+  { id: 3, logo: logo3, number: 12 },
+  { id: 4, logo: logo4, number: 13 },
+  { id: 5, logo: logo5, number: 14 },
+  { id: 6, logo: logo6, number: 15 },
+  { id: 7, logo: logo7, number: 16 },
+  { id: 8, logo: logo8, number: 17 },
+  { id: 9, logo: logo9, number: 18 },
+]
