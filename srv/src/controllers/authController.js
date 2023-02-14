4598e5ae2f0423246015a0e1f6982b0b6df5bf97
editesau <alexey.po121@gmail.com>
2023-02-14 15:11:35 +0300

    Add FS Auth controller functions description

--- srv/src/controllers/authController.js
+++ srv/src/controllers/authController.js
@@ -10,7 +10,7 @@ import { createAccessToken, createRefreshToken } from '../services/jwtService.js
  * 500 - another DB error, body {message}
  * @param req request object from express
  * @param res response to send client
- * @return {void}
+ * @return {Promise<void>}
  */
 export const userCreate = async (req, res) => {
   const { email, password, userName } = req.body
@@ -26,7 +26,15 @@ export const userCreate = async (req, res) => {
     }
   }
 }
-
+/** Function get email and password from request body and invoke check this credentials in DB
+ * send statuses to client:
+ * 200 - user login successful (set-cookie: refresh_token, body {accessToken})
+ * 400 - incorrect email or password body {message}
+ * 500 - another DB error, body {message}
+ * @param req Request Obj
+ * @param res Response Obj
+ * @return {Promise<void>}
+ */
 export const userLogin = async (req, res) => {
   const { email, password } = req.body
   try {
@@ -46,7 +54,15 @@ export const userLogin = async (req, res) => {
     res.status(500).json({ message: dbError.message })
   }
 }
-
+/** Function get userId from request and invoke clear refresh_token for this user in DB
+ * send statuses to client:
+ * 200 - user logout successful, body {message}
+ * 400 - Bad request (refresh_token with provided userId not found
+ * 500 - another DB error, body {message}
+ * @param req Request Obj
+ * @param res Response Obj
+ * @return {Promise<void>}
+ */
 export const userLogout = async (req, res) => {
   try {
     const dbResult = await dbClearRefreshToken(req.userId)
@@ -61,7 +77,16 @@ export const userLogout = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
-
+/** Function get userId from request and invoke generate
+ * new JWT pair and then save new refresh_token for this user in DB
+ * send statuses to client:
+ * 200 - refresh successful, (set-cookie: refresh_token, body {accessToken})
+ * 401 - refresh_token with provided userId not found
+ * 500 - another DB error, body {message}
+ * @param req Request Obj
+ * @param res Response Obj
+ * @return {Promise<void>}
+ */
 export const userAuthRefresh = async (req, res) => {
   const userId = req.userId
   try {
