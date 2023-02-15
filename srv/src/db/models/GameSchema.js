6022893e55560c5876339815f8be37aad0a604ff
editesau <alexey.po121@gmail.com>
2023-02-15 14:29:40 +0300

    add DB GameSchema

---
+++ srv/src/db/models/GameSchema.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const GameSchema = new mongoose.Schema({
+  state: { type: String, default: 'In progress' },
+  cards: { type: [Object], default: [] },
+  userId: { type: String },
+})
+
+export default GameSchema
+/*
+card Object {
+id - random,
+picture - card opened picture
+isOpen - boolean
+isMathced - boolean
+}
+ */
