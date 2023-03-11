df9397c6c62924e1f75bb1a6370ca351739c029d
editesau <alexey.po121@gmail.com>
2023-03-11 18:52:32 +0300

    Delete unused routes and imports

--- srv/src/controllers/gameController.js
+++ srv/src/controllers/gameController.js
@@ -1,6 +1,6 @@
-import { filterCards, generateCards, getTypesDir } from '../helpers/gameLogic/gameLogicFunctions.js'
+import { generateCards, getTypesDir } from '../helpers/gameLogic/gameLogicFunctions.js'
 import {
-  dbCreateGame, dbSetMatched, dbCloseCards, dbResetGame,
+  dbCreateGame, dbResetGame,
 } from '../db/dbActions.js'
 
 export const startGame = async (req, res) => {
@@ -11,40 +11,6 @@ export const startGame = async (req, res) => {
   try {
     const game = await dbCreateGame(userId, cards, gameType)
     res.json({ gameId: game._id, gameType: game.type })
-  } catch (dbError) {
-    res.status(500).json({ message: dbError.message })
-  }
-}
-
-export const closeCards = async (req, res) => {
-  const gameId = req.params.gameId
-  const cardIds = req.body.cardIds
-  const userId = req.userId
-  try {
-    const game = await dbCloseCards(userId, gameId, cardIds)
-    if (game) {
-      const cardToReturn = filterCards(game.cards).find((card) => cardIds.includes(card.id))
-      res.json(cardToReturn)
-    } else {
-      res.status(400).json({ message: 'Cant find data' })
-    }
-  } catch (dbError) {
-    res.status(500).json({ message: dbError.message })
-  }
-}
-
-export const setMatched = async (req, res) => {
-  const gameId = req.params.gameId
-  const userId = req.userId
-  const cardIds = req.body.cardIds
-
-  try {
-    const game = await dbSetMatched(userId, gameId, cardIds)
-    if (game) {
-      res.sendStatus(200)
-    } else {
-      res.status(400).json({ message: 'Cant find data' })
-    }
   } catch (dbError) {
     res.status(500).json({ message: dbError.message })
   }
